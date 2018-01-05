7ca30be7dff8f17991977e94e40aa3e9324282f3
Andy Hanson <anhans@microsoft.com>
2018-01-05 14:30:06 -0800

    add

--- types/dir-resolve/index.d.ts
+++ types/dir-resolve/index.d.ts
@@ -2,6 +2,6 @@
 // Project: https://github.com/mwinche/dir-resolve
 // Definitions by: Andy Hanson <https://github.com/andy-ms>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
+//!
 declare function resolve(path: string): string;
 export = resolve;
